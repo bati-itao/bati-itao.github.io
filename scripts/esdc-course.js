f24b08512aab58b7a124d065e6a8ff7e725a9e9b
Aneta Smulski <smulskianeta@gmail.com>
2022-06-17 07:00:03 -0400

    open details when printing javascript

--- scripts/esdc-course.js
+++ scripts/esdc-course.js
@@ -17,3 +17,25 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+//Open the details when printing
+var beforePrint = function() {
+    $("details").attr('open', '');
+};
+var afterPrint = function() {
+    $("details").removeAttr('open');
+};
+if (window.matchMedia) {
+    var mediaQueryList = window.matchMedia('print');
+    mediaQueryList.addListener(function(mql) {
+        if (mql.matches) {
+            beforePrint();
+        } else {
+            afterPrint();
+        }
+    });
+}
+
+// IE, Firefox
+window.onbeforeprint = beforePrint;
+window.onafterprint = afterPrint;
