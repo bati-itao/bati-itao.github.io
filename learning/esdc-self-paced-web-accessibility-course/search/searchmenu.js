c8155d0001c77909f5ab6f3d8dc38933efd172f2
Aneta Smulski <smulskianeta@gmail.com>
2022-11-17 09:03:02 -0500

    delete old file

--- learning/esdc-self-paced-web-accessibility-course/search/searchmenu.js
+++
@@ -1,7 +0,0 @@
-
-function handleSearchQuery(event) {
-	document.querySelector(".search-error").classList.add("hidden");
-  event.preventDefault();
-  const query =  document.getElementById("search-menu").value.trim().replace(/[^a-zA-Z ]/g, "");
-  alert(query);
-}
